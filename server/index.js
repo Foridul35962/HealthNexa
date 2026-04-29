141f4744695a3d8adbb452023c18ed80dc23da71
Foridul35962 <foridulislam35962@gmail.com>
2026-04-29 15:50:45 +0600

    dockerized client and server

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ import { startServer } from './src/config/redis.js'
 
 const PORT = process.env.PORT || 5000
 
-// startServer().then(()=>{
-    connectDB().then(()=>{
-        app.listen(PORT, ()=>{
+startServer().then(() => {
+    connectDB().then(() => {
+        app.listen(PORT, () => {
             console.log(`server is started at http://localhost:${PORT}`)
         })
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log('mongodb connect failed', err)
     })
-// }).catch((err)=>{
-//     console.log('redis connect failed', err)
-// })
+}).catch((err) => {
+    console.log('redis connect failed', err)
+})
