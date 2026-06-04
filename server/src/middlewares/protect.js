1059ea2f69e26a2412b78e13371e96494fc59642
Foridul35962 <foridulislam35962@gmail.com>
2026-06-04 17:14:12 +0600

    refactor protect middleware to optimize user retrieval with Redis caching

--- server/src/middlewares/protect.js
+++ server/src/middlewares/protect.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import AsyncHandler from "../helpers/AsyncHandler.js";
 import ApiErrors from "../helpers/ApiErrors.js";
 import Users from "../models/Users.model.js";
+import redis from "../config/redis.js";
+import mongoose from "mongoose";
 
 const protect = AsyncHandler(async (req, res, next) => {
     const { token } = req.cookies
@@ -11,7 +13,7 @@ const protect = AsyncHandler(async (req, res, next) => {
 
     let decoded
     try {
-        decoded = await jwt.verify(token, process.env.TOKEN_SECRET)
+        decoded = jwt.verify(token, process.env.TOKEN_SECRET)
     } catch (error) {
         throw new ApiErrors(401, 'unauthenticated access')
     }
@@ -21,9 +23,31 @@ const protect = AsyncHandler(async (req, res, next) => {
     }
 
     const userId = decoded.userId
-    const user = await Users.findById(userId).select('-password')
-    if (!user) {
-        throw new ApiErrors(404, 'user not found')
+    let user
+    const redisKey = `userId:${userId}`
+    const redisUser = await redis.get(redisKey)
+    if (redisUser) {
+        user = JSON.parse(redisUser)
+        user._id = new mongoose.Types.ObjectId(user._id)
+        if (user.hospitalId) {
+            user.hospitalId = new mongoose.Types.ObjectId(user.hospitalId)
+        }
+        if (user.pharmacyId) {
+            user.pharmacyId = new mongoose.Types.ObjectId(user.pharmacyId)
+        }
+    } else {
+        user = await Users.findById(userId)
+            .select('-password')
+            .lean()
+
+        if (!user) {
+            throw new ApiErrors(404, 'user not found')
+        }
+
+        await redis.set(redisKey,
+            JSON.stringify(user),
+            "EX", 300
+        )
     }
 
     req.user = user
